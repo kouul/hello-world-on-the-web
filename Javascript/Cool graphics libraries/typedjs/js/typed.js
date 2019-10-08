8657ee9a449b82fcaf465b08a3a68a7df2d736d6
Akhil <maulloohans@gmail.com>
2019-10-08 08:51:11 +0400

    added second text Welcome

--- Javascript/Cool graphics libraries/typedjs/js/typed.js
+++ Javascript/Cool graphics libraries/typedjs/js/typed.js
@@ -1,12 +1,17 @@
 $('document').ready(function() {
-    var typed = new Typed('#typed', {
+    var helloworld_text = new Typed('#helloworld_text', {
         strings: ['Hello World'],
         stringElement: '.header-text',
         typeSpeed: 50,
         backDelay: 1000,
-        backSpeed: 50,
         showCursor: true,
         loop: true,
         loopCount: Infinity
+    })
+    var randomtext = new Typed('#randomtext', {
+        strings: ['My name is Jonh', 'My name is John', 'Welcome!'],
+        stringElement: '.header-text',
+        typeSpeed: 50,
+        showCursor: true,
     })
 }); 
